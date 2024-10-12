afd9cc37d025bab8aefbd45d89c86e25cab33cb6
Ebi manoj <ebimanoj28@gmail.com>
2024-10-13 02:42:18 +0530

    modal window functionality

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+//////////////////////////////////
+
+const modalBtn = document.querySelectorAll('.btn-open-modal');
+const modalWindow = document.querySelector('.window');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn-close-modal');
+
+// modal window.......
+
+const openModal = function () {
+  modalWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modalWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+modalBtn.forEach(btn => btn.addEventListener('click', openModal));
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modalWindow.classList.contains('hidden'))
+    closeModal();
+});
