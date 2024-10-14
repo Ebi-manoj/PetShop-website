6619f9fe6431b1fff8d2a857144666278c2aed7d
Ebi manoj <ebimanoj28@gmail.com>
2024-10-14 13:19:12 +0530

    stickyNavbar

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 'use strict';
-//////////////////////////////////
+///////////////////////////////////////////////////
 
 const modalBtn = document.querySelectorAll('.btn-open-modal');
 const modalWindow = document.querySelector('.window');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn-close-modal');
+const nav = document.querySelector('.navbar');
+
+////////////////////////////////////////////////////
 
 // modal window.......
 
@@ -25,3 +28,23 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modalWindow.classList.contains('hidden'))
     closeModal();
 });
+
+// creating sticky navbar
+
+const header = document.querySelector('#header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const sticky = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(sticky, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
